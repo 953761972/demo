89f632dd05fea1aff00cf31e77e581812e8cf2e3
xzq <953761972@qq.com>
2021-11-01 16:32:30 +0800

    modify

--- mydemo/src/test/java/com/example/demo/redis/TestRedisson.java
+++ mydemo/src/test/java/com/example/demo/redis/TestRedisson.java
@@ -1,14 +1,18 @@
 package com.example.demo.redis;
 
+import org.junit.Test;
 import org.redisson.Redisson;
 import org.redisson.api.RBucket;
+import org.redisson.api.RLock;
 import org.redisson.api.RedissonClient;
 import org.redisson.config.Config;
+import org.springframework.boot.test.context.SpringBootTest;
 
 /**
  * @Author XZQ
  * @Date 2021/9/1 23:03
  **/
+@SpringBootTest
 public class TestRedisson {
     public static void main(String[] args) {
         Config config = new Config();
@@ -22,5 +26,21 @@ public class TestRedisson {
         bucket.compareAndSet("trySet", "compareAndSetAndSet");
         System.out.println(bucket.getAndSet("getAndSet"));
         System.out.println(bucket.get());
+    }
+
+    @Test
+    public void testLock(){
+        Config config = new Config();
+        config.useSingleServer().setAddress("redis://localhost:6379");
+        RedissonClient redisson = Redisson.create(config);
+        RLock lock=redisson.getLock("redissonLock");
+        System.out.println(lock.tryLock());
+        lock.lock();
+        System.out.println("do something");
+        RBucket<String> bucket = redisson.getBucket("anyObject");
+        String obj = bucket.get();
+        System.out.println(obj.toString());
+        System.out.println("finshed");
+
     }
 }
--- mydemo/src/test/java/com/example/demo/redis/lock/TestRedissonLock.java
+++ mydemo/src/test/java/com/example/demo/redis/lock/TestRedissonLock.java
@@ -15,17 +15,19 @@ public class TestRedissonLock {
     public static void main(String[] args) throws InterruptedException {
         Config config = new Config();
         config.useSingleServer().setAddress("redis://localhost:6379");
-        RedissonClient redisson = Redisson.create(config);
 
         Runnable r1=new Runnable() {
+            RedissonClient redisson = Redisson.create(config);
             @Override
             public void run() {
-                for(int i=0;i<1000;i++){
+                for(int i=0;i<10000;i++){
                     RLock lock = redisson.getLock("Lock");
-                    while(!lock.tryLock()){
-
-                    }
-                    RBucket<String> b = redisson.getBucket("count");
+//                    while(!lock.tryLock()){
+//
+//                    }
+                    lock.lock();
+                    RBucket<String> b = redisson.getBucket("reddsioncount");
+                    //要先删除reddsioncount 否则报：Caused by: java.io.IOException: Unsupported protocol version 51
                     String strCount=b.get();
                     Long count=0l;
                     if(strCount==null || "".equals(strCount)){
@@ -39,16 +41,39 @@ public class TestRedissonLock {
                 }
             }
         };
-
+        Runnable r2=new Runnable() {
+            RedissonClient redisson = Redisson.create(config);
+            @Override
+            public void run() {
+                for(int i=0;i<10000;i++){
+                    RLock lock = redisson.getLock("Lock");
+//                    while(!lock.tryLock()){
+//
+//                    }
+                    lock.lock();
+                    RBucket<String> b = redisson.getBucket("reddsioncount");
+                    String strCount=b.get();
+                    Long count=0l;
+                    if(strCount==null || "".equals(strCount)){
+                        count=0l;
+                    }else{
+                        count=Long.parseLong(strCount);
+                    }
+                    System.out.println(Thread.currentThread().getName()+">>当前："+count);
+                    b.set(String.valueOf(count+1));
+                    lock.unlock();
+                }
+            }
+        };
         Thread t1=new Thread(r1);
-        Thread t2=new Thread(r1);
+        Thread t2=new Thread(r2);
         t1.start();
         t2.start();
         t1.join();
         t2.join();
 
 
-        t2.interrupt();
-        System.out.println("结果："+redisson.getBucket("count").get().toString());
+        RedissonClient redisson = Redisson.create(config);
+        System.out.println("结果："+redisson.getBucket("reddsioncount").get().toString());
     }
 }
